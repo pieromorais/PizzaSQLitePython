218345c1b40fdafaeb812c40cde97d4ca14c4e53
Piero <piero.morais@gmail.com>
2023-02-22 19:52:13 -0300

    First Model: Bordas & Sabores

--- TrabalhoDB/Pizzaria/models.py
+++ TrabalhoDB/Pizzaria/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Bordas(models.Model):
+    # tabela de bordas
+    borda = models.CharField(max_length=200)
+    valor_borda = models.DecimalField(max_digits=2, decimal_places=2)
+
+class Sabores(models.Model):
+    # tabela com sabores
+    sabor = models.CharField(max_length=200)
+    valor_sabor = models.DecimalField(max_digits=2, decimal_places=2)
