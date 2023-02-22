1395d9919d4e0db440051971ba640bab9141280a
Piero <piero.morais@gmail.com>
2023-02-22 20:48:54 -0300

    Changes models

--- TrabalhoDB/Pizzaria/admin.py
+++ TrabalhoDB/Pizzaria/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Sabores, Bordas
 # Register your models here.
+
+admin.site.register(Sabores)
+admin.site.register(Bordas)
--- TrabalhoDB/Pizzaria/models.py
+++ TrabalhoDB/Pizzaria/models.py
@@ -5,9 +5,18 @@ from django.db import models
 class Bordas(models.Model):
     # tabela de bordas
     borda = models.CharField(max_length=200)
-    valor_borda = models.DecimalField(max_digits=2, decimal_places=2)
+    valor_borda = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.borda
+
 
 class Sabores(models.Model):
     # tabela com sabores
     sabor = models.CharField(max_length=200)
-    valor_sabor = models.DecimalField(max_digits=2, decimal_places=2)
+    description = models.CharField(max_length=1000)
+    valor_sabor = models.DecimalField(max_digits=4, decimal_places=2)
+    especial = models.BooleanField(default=0)
+
+    def __str__(self) -> str:
+        return self.sabor
